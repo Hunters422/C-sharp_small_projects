dc69df7bc0407ed146ff49857d894fe63a81f9c3
Hunter <hunterkenneth422@gmail.com>
2020-02-10 12:17:55 -0700

    add code

---
+++ Assignment_pg56/Assignment_pg56/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string x = "Welcome to Acme Accounting Systems!" +
+             Environment.NewLine +
+             "Remeber, we're 'accounting' on you!";
+        Console.Write(x);
+        Console.Read();
+    }
+}
