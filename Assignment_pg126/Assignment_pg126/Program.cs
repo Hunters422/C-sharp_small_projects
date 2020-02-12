d96930f0cc3d68331ec00500406c5e279c570dad
Hunter <hunterkenneth422@gmail.com>
2020-02-12 13:02:58 -0700

    add code assignment 126

---
+++ Assignment_pg126/Assignment_pg126/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 1;
+            //while (x < 100)
+            //{
+            //    Console.WriteLine(x);
+            //    x++;
+            //    Console.ReadLine();
+            //}
+
+            int n = 1;
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            } while (n < 5);
+            Console.ReadLine();
+
+              
+           
+        }
+    }
+}
