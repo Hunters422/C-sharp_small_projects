7a7f4dbbfb49a3d24dc52f9cd330888e67a7dd4d
Hunter <hunterkenneth422@gmail.com>
2020-02-12 13:40:45 -0700

    add code 131

---
+++ Assignment_pg131/Assignment_pg131/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string text = "At the moment, I am concatenating 3 strings" +
+            //            "for my assignment, which" +
+            //            " is on page 131.";
+
+            //Console.WriteLine(text);
+            //Console.ReadLine();
+
+            //string upperExample = "this is me screaming using all caps!";
+            //string upperExample1 = upperExample.ToUpper();
+            //Console.WriteLine(upperExample1);
+            //Console.ReadLine();
+
+            StringBuilder builder = new StringBuilder("Hello Everyone!");
+            builder.Append("This is a my example of using string builder.");
+            builder.AppendLine();
+            builder.Append("So far, I have really enjoyed C#.").AppendLine();
+            builder.AppendLine("I find it very easy to research all of the issues that I have run into.");
+            builder.AppendLine("In addition, my instructor has helped clarify some minor errors I have encountered.");
+            builder.AppendLine("I just want to thank you for taking the time to read this paragraph.");
+            Console.WriteLine(builder);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ StringFunctions/StringFunctions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Hunter";
+            //string quote = "The man said, \\Hello\", Hunter. \n Hello on a new line. \n \t Hello one a tab.";
+            //string fileName = @"C:\Users\Hunter";
+
+            //bool trueOrfalse = name.Contains("s");
+            //trueOrfalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToLower();
+
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            string name = "Hunter";
+            name = "Janko";
+
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+        }
+    }
+}
