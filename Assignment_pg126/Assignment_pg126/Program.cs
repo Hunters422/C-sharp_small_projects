0cd06b1475edf9790a521d60baebaf17f9d54b2f
Hunter <hunterkenneth422@gmail.com>
2020-02-13 15:35:31 -0700

    update code for assignments

--- Assignment_pg126/Assignment_pg126/Program.cs
+++ Assignment_pg126/Assignment_pg126/Program.cs
@@ -10,21 +10,59 @@ namespace Assignment_pg126
     {
         static void Main(string[] args)
         {
-            //int x = 1;
-            //while (x < 100)
-            //{
-            //    Console.WriteLine(x);
-            //    x++;
-            //    Console.ReadLine();
-            //}
 
-            int n = 1;
+            //bool isTrue = true;
+            //while (isTrue)
+            //{
+            //    Console.WriteLine(isTrue);
+            //    isTrue = false;
+            //    Console.WriteLine(isTrue);
+            //}
+            //Console.ReadLine();
+
+            Console.WriteLine("Pick a number between 1-10: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool numberGuess = number == 6;
+
             do
             {
-                Console.WriteLine(n);
-                n++;
-            } while (n < 100);
-            Console.ReadLine();                    
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("1? Wrooooong. Try Again.");
+                        Console.WriteLine("Guess a number: ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("2? Wrooooong. Try Again.");
+                        Console.WriteLine("Guess a number: ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("3? Wrooooong. Try Again.");
+                        Console.WriteLine("Guess a number: ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("4? Wrooooong. Try Again.");
+                        Console.WriteLine("Guess a number: ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 7:
+                        Console.WriteLine("7! CORRECT!! FINALLY!!...if you want to guess again be my guest...");
+                        Console.WriteLine("Guess a number: ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Ugh, keep trying!");
+                        Console.WriteLine("Guess a number: ");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            while (!numberGuess);
+            Console.ReadLine();
         }
     }
 }
