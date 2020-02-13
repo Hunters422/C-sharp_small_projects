bf9507f903549565ac2ba8e98a2b1f530befe2aa
Hunter <hunterkenneth422@gmail.com>
2020-02-12 23:15:39 -0700

    add code

---
+++ Assignment_pg136/Assignment_pg136/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main()
+        {
+        string[] strArray = { "Harry", "Potter", "Ron", "Weasley" };
+        Console.WriteLine("Please select an index of the Array: ");
+        string selectArray = Console.ReadLine();
+        Console.WriteLine(selectArray[0]);
+        Console.ReadLine();
+        }
+    }
