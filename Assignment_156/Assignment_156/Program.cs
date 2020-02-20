237cc769790f9536bc9e090811c18b3d00c52753
Hunter <hunterkenneth422@gmail.com>
2020-02-19 23:13:37 -0700

    add code

---
+++ Assignment_156/Assignment_156/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass a = new myClass();
+            Console.WriteLine("Please select a random number to perform 3 basic math operations on: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The number you selected plus 10 equals: " + a.addTwo(x));
+            Console.WriteLine("The number you selected times 10 equals: " + a.timesTwo(x));
+            Console.WriteLine("The number you selected divided by 10 equals: " + a.divideTwo(x));
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Assignment_156/Assignment_156/myClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_156
+{
+    class myClass
+    {
+        public int addTwo(int x)
+        {
+            return x + 10;
+        }
+        public int timesTwo(int x)
+        {
+            return x * 10;
+
+        }
+        public int divideTwo(int x)
+        {
+            return x / 10;
+        }
+ 
+
+    }
+}
