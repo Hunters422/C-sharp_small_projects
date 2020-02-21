5e9ec9888573929e8a9af1f38014c0b5b2f72679
Hunter <hunterkenneth422@gmail.com>
2020-02-21 11:53:46 -0700

    add code 159

---
+++ Assignment_pg159/Assignment_pg159/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass a = new myClass();
+
+            Console.WriteLine("Please type in a number to add to 10: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int result1 = a.addMethod(x);
+            Console.WriteLine("The number you selected plus 10 equals: " + result1);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Please type in a decimal to times by 10: ");
+            decimal y = Convert.ToDecimal(Console.ReadLine());
+            int result2 = a.addMethod(y);
+            Console.WriteLine("The number you selected times 10 equals: " + result2);
+            Console.ReadLine();
+
+            Console.WriteLine("Please type in a string to convert to an int and divide by 10: ");
+            string input = Console.ReadLine();
+            int result3 = a.addMethod(input);
+            Console.WriteLine("Please spell a number to convert to a integer and divide by 10: " + result3);           
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Assignment_pg159/Assignment_pg159/myClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg159
+{
+    public class myClass
+    {
+        public int addMethod(int x)
+        {
+            int result = x + 10;
+            return result;
+
+        }
+        public int addMethod(decimal y)
+        {
+            //Console.WriteLine("Please type in a decimal number: ");
+            //decimal y = Convert.ToDecimal(Console.ReadLine());
+
+            int result2 = Convert.ToInt32(y * 10);
+            return result2;
+
+        }
+        public int addMethod(string input)
+        {
+            //Console.WriteLine("Please spell a number to convert to a integer and divide by 10: ");
+
+            int result3 = Convert.ToInt32(input) / 10;
+            return result3;
+        }
+    }
+}
+
