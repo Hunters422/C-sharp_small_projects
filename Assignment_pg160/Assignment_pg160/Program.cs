a4e289e2703b8a2e0f768183f196c0818294ff37
Hunter <hunterkenneth422@gmail.com>
2020-02-23 10:52:32 -0700

    add code 160

---
+++ Assignment_pg160/Assignment_pg160/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg160
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass a = new myClass();
+
+            //Console.WriteLine("Please input two numbers. We are going to take those numbers, add them together, and then add 15. First Number: ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Second Number: ");
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //int result = a.addMethod(x,y);
+            //Console.WriteLine(x + y + " plus 15 equals " + result);
+            //Console.ReadLine();
+
+            Console.WriteLine("Please type enter in a number. You can type in a second number if you wish but you may leave it blank.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("And second number if you choose: ");
+            if (string.IsNullOrEmpty(Console.ReadLine()))
+            {
+                int y = Convert.ToInt32(0);
+                int result = a.addMethod(x, y);
+                Console.WriteLine("Your number(s) plus 15 equals " + result);
+                Console.ReadLine();
+
+
+            } 
+            else
+            {
+                int y = Convert.ToInt32(Console.ReadLine());
+                int result = a.addMethod(x, y);
+                Console.WriteLine("Your number(s) plus 15 equals " + result);
+                Console.ReadLine();
+
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Assignment_pg160/Assignment_pg160/myClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg160
+{
+    class myClass
+    {
+        public int addMethod(int x, int y)
+        {
+            int result = x + y + 15;
+            return result;
+
+        }
+
+    }
+}
