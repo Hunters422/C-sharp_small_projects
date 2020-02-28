bcc8f50375a5f71a5fe5f4edf5832a107c8c02a5
Hunter <hunterkenneth422@gmail.com>
2020-02-28 13:02:58 -0700

    add code

---
+++ Assignment_pg179/Assignment_pg179/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg179
+{
+    public class Employee : Assignment_pg179.IQuittable
+
+    {
+        public void Quit()
+        {
+            Console.Write("I quit");
+        }
+    }
+}
---
+++ Assignment_pg179/Assignment_pg179/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg179
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable employee = new Employee();
+            employee.Quit();
+            Console.ReadLine();
+        }
+    }
+}
