771ab2250aa42a6bd2398c62f09c6dfce7493e4e
Hunter <hunterkenneth422@gmail.com>
2020-02-28 13:47:26 -0700

    add code

---
+++ Assignment_pg197/Assignment_pg197/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg197
+{
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                Number number = new Number();
+                number.Amount = 50;
+                Console.WriteLine(number.Amount);
+                Console.ReadLine();
+
+            }
+            struct Number
+            {
+                public decimal Amount { get; set; }
+            }
+        }
+}
