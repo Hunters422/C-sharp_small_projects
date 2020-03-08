54a49c71ccbd7575829ed282cb4a85f6d11bc153
Hunter <hunterkenneth422@gmail.com>
2020-03-08 07:03:34 -0600

    add code

---
+++ Assignment_pg221/Assignment_pg221/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine("The current Date and Time is : " + currentTime);
+            Console.WriteLine("Please enter a number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("It will be " + currentTime.AddHours(input) + " in " + input + " hours ");
+            Console.WriteLine("Also, time is just in illuuuuuusion. Oof..spooky 0.o");
+            Console.ReadLine();
+        }
+    }
+}
