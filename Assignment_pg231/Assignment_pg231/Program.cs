52af2f1dfc4ad99dcb7680275caaa335af759d49
Hunter <hunterkenneth422@gmail.com>
2020-03-09 16:14:11 -0600

    add code

---
+++ Assignment_pg231/Assignment_pg231/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg231
+{
+    class Program
+    {
+        private string greeting;
+
+        static void Main(string[] args)
+        {
+            const string greeting = "Everybody was,";
+            string var = "KUNGFOO FIGHTIN";
+            Console.WriteLine(greeting + var);
+            Console.ReadLine();
+        }
+        public Program(string greeting, string var) : this(greeting)
+        {
+
+            var = greeting;
+        }
+
+        public Program(string greeting)
+        {
+            this.greeting = greeting;
+        }
+    }
+}
